00911ed5f765eb1d66026d674b3184a85f8847d9
Anastasia <kop.anastasia27@gmail.com>
2026-02-03 10:28:20 +0200

    add navigation to the main rout in header

--- src/pages/ico/components/ICOHeader.tsx
+++ src/pages/ico/components/ICOHeader.tsx
@@ -1,7 +1,14 @@
 import { ICO_CONFIG} from '@/constants/ico';
 import { MainButton } from './shared/MainButton';
+import { useNavigate } from 'react-router-dom';
+
 
 export function ICOHeader() {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/ico');
+  }
 
   return (
     <header className="relative border-b h-28 z-50 border-[hsl(var(--ico-border-color))] bg-[hsl(var(--ico-bg-secondary))] shadow-md shadow-slate-900">
@@ -10,7 +17,7 @@ export function ICOHeader() {
           {/* Logo & Token Name */}
           <div className="flex items-center gap-3">
             <div className="w-20 h-20  ">
-              <img src="/leaseFiLogo.png" alt="Token Logo" />
+              <img src="/leaseFiLogo.png" alt="Token Logo" onClick={handleLogoClick} className="cursor-pointer" />
             </div>
             <div>
               <h1 className="text-xl font-bold text-[hsl(var(--ico-text-primary))]">
