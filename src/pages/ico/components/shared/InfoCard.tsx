6264f82f7c9fdf0e1cad8f2a8ace4ce101c0822e
Anastasia <kop.anastasia27@gmail.com>
2026-02-10 09:43:47 +0200

    update cards styles

--- src/pages/ico/components/shared/InfoCard.tsx
+++ src/pages/ico/components/shared/InfoCard.tsx
@@ -10,7 +10,7 @@ export function InfoCard({ children, className }: InfoCardProps) {
   return (
     <div
       className={cn(
-        'grid grid-cols-1 bg-black rounded-xl md:grid-cols-4 z-20',
+        'grid grid-cols-1 bg-[hsl(var(--ico-bg-card))] rounded-md md:grid-cols-4 z-20 border border-[hsl(var(--ico-border-color))]',
         className
       )}
     >
