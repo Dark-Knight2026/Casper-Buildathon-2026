a04c12accdc07346fbb5e3641d1940f288b74bb0
ksmit323 <ksmit323@gmail.com>
2026-03-11 20:35:20 +0800

    fix: Restructure tests into separate files under a new tests/ directory

--- smart_contracts/src/lib.rs
+++ smart_contracts/src/lib.rs
@@ -1,16 +1,20 @@
 #![cfg_attr(not(test), no_std)]
 #![cfg_attr(not(test), no_main)]
 extern crate alloc;
+extern crate self as leasefi_contracts;
 
 pub mod common;
 pub mod constants;
 pub mod escrow;
 pub mod ico;
 pub mod lease;
-mod mocks;
+pub mod mocks;
 pub mod nft;
 pub mod roles;
 pub mod staking;
 pub mod tailor_coin;
 pub mod treasury;
 pub mod vesting;
+
+#[cfg(test)]
+mod tests;
---
+++ smart_contracts/src/tests.rs
@@ -0,0 +1,18 @@
+#[path = "../tests/escrow_tests.rs"]
+mod escrow_tests;
+#[path = "../tests/ico_tests.rs"]
+mod ico_tests;
+#[path = "../tests/lease_tests.rs"]
+mod lease_tests;
+#[path = "../tests/nft_tests.rs"]
+mod nft_tests;
+#[path = "../tests/roles_tests.rs"]
+mod roles_tests;
+#[path = "../tests/staking_tests.rs"]
+mod staking_tests;
+#[path = "../tests/tailor_coin_tests.rs"]
+mod tailor_coin_tests;
+#[path = "../tests/treasury_tests.rs"]
+mod treasury_tests;
+#[path = "../tests/vesting_tests.rs"]
+mod vesting_tests;
