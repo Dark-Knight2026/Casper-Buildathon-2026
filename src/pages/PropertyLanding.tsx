f6563b201dd32a54a6fd39a359484c531cf4b8f6
Anastasia <kop.anastasia27@gmail.com>
2026-04-07 10:29:19 +0300

    add header and update PropertyLanding page

--- src/pages/PropertyLanding.tsx
+++ src/pages/PropertyLanding.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { useNavigate } from 'react-router-dom';
 import FeaturedProperties from '@/components/FeaturedProperties';
+import LandingHeader from '@/components/LandingHeader';
 import {
   Search,
   Home,
@@ -59,15 +60,17 @@ export default function PropertyLanding() {
 
   return (
     <div className="min-h-screen bg-secondary">
+      <LandingHeader />
       {/* Hero Section */}
-      <section className="relative overflow-hidden pt-20 pb-32 border-b border-border">
-        <div className="absolute inset-0" style={{ backgroundImage: "url('/images/ModernApartment.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "30% center" }} />
+      <section className="relative overflow-hidden pt-16 pb-20 border-b border-border">
+        <div className="absolute inset-0" style={{ backgroundImage: "url('/images/ModernApartment.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center 30%" }} />
         <div className="absolute inset-0 bg-primary/75" />
-        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center min-h-full pt-20
+        ">
           <div className={`text-center transition-all duration-1000 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>
-            <Badge className="mb-4 bg-white/20 text-white border-white/30">
+            {/* <Badge className="mb-4 mt-4 bg-white/20 text-white border-white/30">
               New Enhanced Features Available
-            </Badge>
+            </Badge> */}
             <h1 className="text-5xl md:text-7xl font-bold text-white mb-6 leading-tight">
               Find Your{' '}
               <span className="text-white/80">Dream Home</span>
